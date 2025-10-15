cc37b3678b1dc3da81c024636daadda781ce6f2e
azuaje301 <jose_diazgranado@estudante.sesisenai.org.br>
2025-10-14 21:56:16 -0300

    trabalho python

---
+++ jose_NOME_ESTRUTURA_DECISAO_25.py
@@ -0,0 +1,11 @@
+#faça um programa para a leitura de duas notas parciais de um aluno.O programa deve calculare a média alcançada por aluno e apresentar:
+numero_1 =float(input("escreva numero_1: "))
+numero_2 =float(input("escreva numero_2: "))
+
+if numero_1 > 7:
+    print(f"aprovado {numero_1} se a media alcançada for maior ou igual a sete")
+elif numero_2 < 7:
+    print(f"reprovado{numero_2} se a media for menor do que sete")
+ 
+media = (numero_1 + numero_2) /2
+print(f"media{media}")
