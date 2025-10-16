e2e62d2b50fea8fe3deaff24f63736de9109b1e0
azuaje301 <jose_diazgranado@estudante.sesisenai.org.br>
2025-10-15 21:36:03 -0300

    trabalho de python

---
+++ jose_NOME_ESTRUTURA_REPETICAO_3.py
@@ -0,0 +1,12 @@
+#Faça um programa que leia 5 números e informe a soma e a média dos números.
+numero_1 = float(input("escreva numero_1 :"))
+numero_2 = float(input("escreva numero_2 :"))
+numero_3 = float(input("escreva numero_3 :"))
+numero_4 = float(input("escreva numero_4 :"))
+numero_5 = float(input("escreva numero_5 :"))
+
+soma = (numero_1 + numero_2 + numero_3 + numero_4 + numero_5 ) 
+print(f"soma {soma}")
+
+media = (numero_1 + numero_2 + numero_3 + numero_4 + numero_5 ) /5
+print(f"media{media}")
